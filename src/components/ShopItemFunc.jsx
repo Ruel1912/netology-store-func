f54e1ac6c90483ddf163ac04bb52b4542b78aa2c
Mikhail Katagarov <mikatagarov@gmail.com>
2024-10-16 15:22:39 +0300

    Add props validate

--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,16 +1,29 @@
+import PropTypes from 'prop-types';
+
 const ShopItemFunc = ({ item }) => {
-  const {brand, title, description, descriptionFull, price, currency} = item 
+  const {brand, title, description, descriptionFull, price, currency} = item;
+
+  ShopItemFunc.propTypes = {
+    item: PropTypes.shape({
+      brand: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      descriptionFull: PropTypes.string,
+      price: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired
+    }).isRequired
+  };
 
   return (
-    <div class="main-content">
+    <div className="main-content">
       <h2>{brand ?? 'Производитель'}</h2>
       <h1>{title ?? 'Название товара'}</h1>
       <h3>{description ?? 'Краткое описание'}</h3>
-      <div class="description">{descriptionFull ?? 'Описание товара'}</div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{currency}{price.toFixed(2) ?? 0}</div>
+      <div className="description">{descriptionFull ?? 'Описание товара'}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price.toFixed(2) ?? 0}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
